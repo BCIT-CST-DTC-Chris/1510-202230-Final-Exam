ba39857c3e7d31daba11d95e210e6dbee42432a1
Christopher Thompson <chris_thompson@bcit.ca>
2022-12-08 10:28:05 -0800

    I think we're ready!

---
+++ hard.py
@@ -0,0 +1,35 @@
+"""
+
+FUNCTION:
+_________
+
+Define a function called NAME.
+
+This function DOES
+
+ASSUMPTIONS (PRECONDITIONS):
+____________
+
+IBID
+
+DOCSTRING:
+__________
+
+No. No docstrings are needed.
+
+DOCTESTS:
+_________
+
+No. No doctests are needed.
+
+UNIT TESTS:
+___________
+
+No. No unit tests are needed.
+
+MAIN FUNCTION AND IF-STATEMENT BELOW IT
+_______________________________________
+
+Yes. The main function should contain the code in the examples to demo your code.
+
+"""
